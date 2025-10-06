cc923ced152b0ffffa7b3efb3a71125829cba466
Ivan Tymchyna <icq437699228@gmail.com>
2025-10-06 14:02:26 +0300

    init

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-     # роутери
-        from .routes import main_bp
-        app.register_blueprint(main_bp)
-
+    # роутери
+    from .routes import main_bp
+    app.register_blueprint(main_bp)
 
     # логування
     from .logs import logs_bp
