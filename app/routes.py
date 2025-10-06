01600133f7c6dd875e17556cff0a68b6114a8d3b
Ivan Tymchyna <icq437699228@gmail.com>
2025-10-06 14:27:42 +0300

    init

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, jsonify
 import requests
 from threading import Thread
 import time
+import os
+from datetime import datetime
 
 main_bp = Blueprint('main', __name__)
 
@@ -10,6 +12,17 @@ PROXIES = {
     4444: {"iface": "eth1", "user": "charter", "pass": "tickets2025", "public_ip": "0.0.0.0"},
     4445: {"iface": "eth0", "user": "charter", "pass": "tickets2025", "public_ip": "0.0.0.0"},
 }
+
+
+
+def get_log_file_path():
+    today_str = datetime.now().strftime("%d%m%y")
+    path = os.path.join("/var/log/3proxy", f"3proxy-{today_str}.log")
+    # створюємо файл, якщо його нема
+    if not os.path.exists(path):
+        open(path, 'a').close()
+    return path
+
 
 def get_public_ip(proxy_port, username, password):
     proxy_url = f"http://{username}:{password}@127.0.0.1:{proxy_port}"
@@ -32,6 +45,7 @@ thread.start()
 
 @main_bp.route('/')
 def index():
+    log_path = get_log_file_path()
     return render_template("index.html", proxies=PROXIES, log_path=log_path)
 
 # API для автопідвантаження IP через JS
@@ -39,13 +53,3 @@ def index():
 def api_proxies():
     return jsonify(PROXIES)
 
-import os
-from datetime import datetime
-
-def get_log_file_path():
-    today_str = datetime.now().strftime("%d%m%y")
-    path = os.path.join("/var/log/3proxy", f"3proxy-{today_str}.log")
-    # створюємо файл, якщо його нема
-    if not os.path.exists(path):
-        open(path, 'a').close()
-    return path
